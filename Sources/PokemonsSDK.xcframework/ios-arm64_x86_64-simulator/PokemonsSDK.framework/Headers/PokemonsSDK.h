7df3e912293cd50364b53e751e19c7a962e88bf4
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2024-10-09 22:36:30 -0400

    Initial commit

---
+++ Sources/PokemonsSDK.xcframework/ios-arm64_x86_64-simulator/PokemonsSDK.framework/Headers/PokemonsSDK.h
@@ -0,0 +1,16 @@
+//
+//  PokemonsSDK.h
+//  PokemonsSDK
+//
+//  Created by Mukhtar Yusuf on 7/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokemonsSDK.
+FOUNDATION_EXPORT double PokemonsSDKVersionNumber;
+
+//! Project version string for PokemonsSDK.
+FOUNDATION_EXPORT const unsigned char PokemonsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokemonsSDK/PublicHeader.h>
